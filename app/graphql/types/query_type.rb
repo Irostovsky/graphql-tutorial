a5233b491fd3295d0ab6e241773764e19fbb7a56
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2019-04-11 19:06:41 +0300

    Create links by mutations.

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,15 +1,19 @@
 # frozen_string_literal: true
 
 module Types
-  class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+  class QueryType < BaseObject
+    # queries are just represented as fields
+    # `all_links` is automatically camelcased to `allLinks`
+    field :all_links, [LinkType], null: false
+    field :first_link, [LinkType], null: false
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-                               description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    # this method is invoked, when `all_link` fields is beeing resolved
+    def all_links
+      Link.all
+    end
+
+    def first_link
+      [Link.first]
     end
   end
 end
