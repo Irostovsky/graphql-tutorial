efcadb33aea37f6814fa482142f03e1f40c834ba
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2019-04-12 16:21:04 +0300

    Auth user.

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,6 +7,7 @@ module Types
     field :all_links, [LinkType], null: false
     field :first_link, [LinkType], null: false
     field :users, [UserType], null: false
+    field :me, UserType, null: true
 
     # this method is invoked, when `all_link` fields is beeing resolved
     def all_links
@@ -19,6 +20,16 @@ module Types
 
     def users
       User.all
+    end
+
+    def me
+      token = context[:session][:token]
+      return unless token
+
+      crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
+      token = crypt.decrypt_and_verify context[:session][:token]
+      user_id = token.gsub('user-id:', '').to_i
+      User.find_by id: user_id
     end
   end
 end
